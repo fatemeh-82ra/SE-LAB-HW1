03d775faea8643c8e45b8092a48e2f240a0bd676
fatemeh <fati20khoji@gmail.com>
2025-07-24 20:17:48 +0330

    Implement number and decimal button functionality

--- calculator.js
+++ calculator.js
@@ -2,3 +2,12 @@ const display = document.querySelector('#display');
 
 const buttons = document.querySelectorAll('button');
 
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.textContent;
+
+        if (buttonText >= '0' && buttonText <= '9' || buttonText === '.') {
+            display.value += buttonText;
+        }
+    });
+});
