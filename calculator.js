71302174e3e1b42fa8ca2e6d80bd2369d2c17021
fatemeh <fati20khoji@gmail.com>
2025-07-24 20:25:30 +0330

    Implement basic operator and equals button logic

--- calculator.js
+++ calculator.js
@@ -5,16 +5,14 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         const buttonText = button.textContent;
 
-        if (buttonText >= '0' && buttonText <= '9' || buttonText === '.') {
-            display.value += buttonText;
-        }
-
         if (buttonText === 'AC') {
             display.value = '';
-        }
-
-        if (buttonText === '⌫') {
+        } else if (buttonText === '⌫') {
             display.value = display.value.slice(0, -1);
+        } else if (buttonText === '=') {
+            display.value = eval(display.value);
+        } else {
+            display.value += buttonText;
         }
     });
 });
